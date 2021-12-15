e35efe69720d8d5c065ae8a6270d85da370b9c27
Aleex55 <94048476+Aleex55@users.noreply.github.com>
2021-12-15 15:52:03 +0100

    ex

---
+++ Ex5.js
@@ -0,0 +1,13 @@
+var aux = '';
+document.onkeydown=function(letter){
+    if (letter.key == 'Backspace'){
+        aux = '';
+    }else {
+        aux = aux + letter.key;
+    }
+    document.getElementById('letra').innerHTML = aux;
+    if (aux.includes("esketit")){
+        window.open("https://www.youtube.com/watch?v=iSIcWKEVQIw&t=0s");
+    }
+
+}
